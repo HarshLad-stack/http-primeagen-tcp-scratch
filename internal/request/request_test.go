6d99ead99c065ce42b335625f76c72097fa35685
HarshLad-stack <harshlad757@gmail.com>
2026-02-19 12:09:51 +0530

    feat: setup internal request package and verify testify testing

---
+++ internal/request/request_test.go
@@ -0,0 +1,13 @@
+package request
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRequestLineParse(t *testing.T) {
+	// This just checks if "A" equals "A".
+	// It proves the Robot is awake!
+	assert.Equal(t, "TheTestagen", "TheTestagen")
+}
